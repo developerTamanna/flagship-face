e9daaf662e0f24107d6b2582fccac89560307424
developerTamanna <astamanna57@gmail.com>
2025-04-19 20:55:10 +0600

    nested sajano

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import { Outlet } from 'react-router'
 import './App.css'
 
 function App() {
 
   return (
  <>
-   <h1>hii</h1>
+   <h1>Navbar</h1>
+   
+   <Outlet></Outlet>
+
+   <h1>Footer</h1>
+         {/* placeholder? */}
+
 
  </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
+
+
 import { StrictMode } from 'react'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Favorite from './Favorite.jsx';
 
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children:[{
+      path: "/favorite",
+      Component:Favorite,
+      children:[{
+           path: 'more-nested',
+           element: <p>more nested</p>,
+      },
+    ],
+    },
+    {
+      path: "/about",
+      element: 
+       
+       <p>this is a about </p>
+       
+     
+     },
+  
+  
+  ],
+  },
+ 
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
