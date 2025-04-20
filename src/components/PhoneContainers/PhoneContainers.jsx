b5cc7e78e68788377b24e4e7201ac1c6ffa74633
developerTamanna <astamanna57@gmail.com>
2025-04-20 12:15:20 +0600

    show and all button click fo card

--- src/components/PhoneContainers/PhoneContainers.jsx
+++ src/components/PhoneContainers/PhoneContainers.jsx
@@ -4,15 +4,22 @@ import PhoneCard from '../PhoneCard/PhoneCard';
 
 const PhoneContainers = ({phones}) => {
 
-    const [displayPhones, setDisplayPhones] = useState([])
+    const [displayPhones, setDisplayPhones] = useState([]);
 
+   //button er jnno
 
+   const [showAll, setShowAll] = useState(false);
 //useEffect
 
 useEffect(()=>{
 
+  if(showAll){
+    setDisplayPhones(phones)
+  }
+  else{
     setDisplayPhones(phones.slice(0, 6))
-},[])
+  }
+},[phones, showAll])
 
     // console.log(phones)
     return (
@@ -36,14 +43,18 @@ useEffect(()=>{
         }
             
             </div>
-            <a href="#_" className="relative inline-block text-lg group">
-    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-        <span className="relative">Show All</span>
-    </span>
-    <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-</a>
+            <button onClick={()=>{
+                setShowAll(prv => !prv)
+                if(showAll) window.scrollTo({ top: 0, behavior: "smooth" });
+            }}  className="relative inline-block text-lg group">
+  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+    <span className="relative">{showAll? 'show less': 'show all'}</span>
+  </span>
+  <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</button>
+
         </div>
     );
 };
